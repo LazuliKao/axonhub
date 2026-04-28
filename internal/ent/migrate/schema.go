c8a6b31a0a0a421fe5fdbbe2619c10b10bdfb82c
LazuliKao <LazuliKao233@outlook.com>
2026-04-28 23:09:04 +0800

    Revert "fix"
    This reverts commit 11997cc9c02d06666036653cf54d9b84fca4c8bc.

--- internal/ent/migrate/schema.go
+++ internal/ent/migrate/schema.go
@@ -595,14 +595,9 @@ var (
 				Columns: []*schema.Column{RequestExecutionsColumns[20], RequestExecutionsColumns[12], RequestExecutionsColumns[1]},
 			},
 			{
-				Name:    "request_executions_by_request_id_created_at",
-				Unique:  false,
-				Columns: []*schema.Column{RequestExecutionsColumns[20], RequestExecutionsColumns[1]},
-			},
-			{
 				Name:    "request_executions_by_channel_id_created_at",
 				Unique:  false,
-				Columns: []*schema.Column{RequestExecutionsColumns[18], RequestExecutionsColumns[1]},
+				Columns: []*schema.Column{RequestExecutionsColumns[18]},
 			},
 		},
 	}
